11b4f214e169606d62d54ff4656d03f1d5a33be5
Emma Egstad <emma@egstad.com>
2022-02-21 12:14:17 -0800

    updating label

--- src/views/Auth/Login.jsx
+++ src/views/Auth/Login.jsx
@@ -28,14 +28,14 @@ export default function Login() {
     <>
       <h3>You must log in to view the page at {from.pathname}</h3>
       <form onSubmit={handleLogin} className={styles.loginForm}>
-        <label for="email">Email</label>
+        <label htmlFor="email">Email</label>
         <input
           id="email"
           name="email"
           type="email"
           onChange={(e) => handleFormChange(e)}
         />{' '}
-        <label for="password">Password</label>
+        <label htmlFor="password">Password</label>
         <input
           id="password"
           name="password"
